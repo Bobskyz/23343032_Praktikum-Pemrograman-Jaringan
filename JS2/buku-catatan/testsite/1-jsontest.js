281ee4d1e58f61093a666178d7c7846431a54a5f
Dafin Surya <145106018+Bobskyz@users.noreply.github.com>
2025-10-01 21:14:44 +0700

    Add files via upload

---
+++ JS2/buku-catatan/testsite/1-jsontest.js
@@ -0,0 +1,13 @@
+/*
+const buku = {
+    judul: 'Pemrograman Jaringan',
+    penulis: 'Dafin Surya'
+}
+const bookJSON = JSON.stringify(buku);
+fs.writeFileSync('1-jsontest.json', bookJSON);
+*/
+const fs = require('fs');
+const dataBuffer = fs.readFileSync('1-jsontest.json');
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data.judul);
